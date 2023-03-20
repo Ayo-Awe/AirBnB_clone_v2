cd62ebb392ab64946f2490edeb4a369c7203f36e
Awe Ayomidipupo <pupoawe@gmail.com>
2023-03-20 20:55:10 +0100

    chore: add scripts to setup project databases

---
+++ setup_mysql_test.sql
@@ -0,0 +1,5 @@
+-- Script to setup mysql test database
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL ON hbnb_test_db.* TO  'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO  'hbnb_test'@'localhost';
