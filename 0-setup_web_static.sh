53f84805d3da72832c923426c4a7c2c0c2fa30b3
Awe Ayomidipupo <pupoawe@gmail.com>
2023-04-05 14:45:45 +0100

    fix: edit nginx config not replace

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -15,32 +15,6 @@ echo "<html>
 rm -f /data/web_static/current
 ln -s /data/web_static/releases/test/ /data/web_static/current
 chown ubuntu:ubuntu -R /data/
-echo "server {
-        listen 80 default_server;
-        listen [::]:80 default_server;
-
-        root /var/www/html;
-
-        index index.html index.htm index.nginx-debian.html;
-
-        server_name _;
-        add_header X-Served-By \$hostname always;
-
-        error_page 404 /404.html;
-        location /404.html {
-                 root /usr/share/nginx/html;
-        }
-
-        location /redirect_me {
-                return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-        }
-
-        location /hbnb_static/ {
-				alias /data/web_static/current/;
-        }
-
-        location / {
-                try_files \$uri \$uri/ =404;
-        }
-}" > /etc/nginx/sites-enabled/default
+newlines="server_name _;\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}"
+sed -i "s~server_name _;~$newlines~" /etc/nginx/sites-enabled/default
 service nginx restart
