7534e9b98589ed4f078c3536df5f19073c79d82f
Awe Ayomidipupo <pupoawe@gmail.com>
2023-04-05 14:24:13 +0100

    fix: setup dummy html content

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,7 +5,14 @@ apt-get update -y
 apt-get install -y nginx
 mkdir -p  /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-echo "Hello World" > /data/web_static/releases/test/index.html
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>
+" > /data/web_static/releases/test/index.html
 rm -f /data/web_static/current
 ln -s /data/web_static/releases/test/ /data/web_static/current
 chown ubuntu:ubuntu -R /data/
