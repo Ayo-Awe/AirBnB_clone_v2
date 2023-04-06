1036969624988f47827d4c9053ccfc6f500ffc25
Awe Ayomidipupo <pupoawe@gmail.com>
2023-04-06 10:35:37 +0100

    feat: add script to package web_static files

---
+++ 1-pack_web_static.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Generate a .tgz archive from the
+contents of the web_static folder
+"""
+from fabric.api import *
+from datetime import datetime
+
+
+def do_pack():
+    """Generate a .tgz archive from the
+    contents of the web_static folder
+    """
+    date_format = datetime.now().strftime("%Y%m%d%H%M%S")
+    file_path = f"versions/web_static_{date_format}.tgz"
+    local("mkdir -p versions")
+    archive = local(f"tar -cvzf {file_path} web_static")
+
+    if archive.failed:
+        return None
+
+    return file_path
