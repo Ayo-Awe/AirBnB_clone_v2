7de3652c2d893db8845c9f5769773ed2780fc696
Awe Ayomidipupo <pupoawe@gmail.com>
2023-04-06 13:38:28 +0100

    fix: delete archive file from /tmp once done

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -41,13 +41,14 @@ def do_deploy(archive_path):
     temp_archive, = put(local_path=archive_path, remote_path="/tmp")
     folder_name = "/data/web_static/releases/{}".format(archive_name)
     r1 = sudo("mkdir -p {}".format(folder_name))
-    r2 = sudo("tar -xzvf {} -C {}".format(temp_archive, folder_name))
+    r2 = sudo("tar -xzf {} -C {}".format(temp_archive, folder_name))
+    r6 = sudo("rm {}".format(temp_archive))
     r3 = sudo("mv {}/web_static/* {}".format(folder_name, folder_name))
     r4 = sudo("rm -rf {}/web_static".format(folder_name))
-    r5 = sudo("rm -r  /data/web_static/current".format(folder_name))
-    r6 = sudo("ln -s {} /data/web_static/current".format(folder_name))
+    r5 = sudo("rm -rf  /data/web_static/current".format(folder_name))
+    r7 = sudo("ln -s {} /data/web_static/current".format(folder_name))
 
     if r1.failed and r2.failed and r3.failed and r4.failed and r5.failed \
-            and r6.failed:
+            and r6.failed and r7.failed:
         return False
     return True
