83247e922f5f43c6c6dab5e1c691edefbc0877da
Awe Ayomidipupo <pupoawe@gmail.com>
2023-04-06 12:58:21 +0100

    fix: add code from task 1

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,12 +3,29 @@
 to web servers. Uploads the new archive
 and configures the new release on the server
 """
+from datetime import datetime
 import os
-from fabric.api import env, put, sudo
+from fabric.api import env, put, sudo, local
 
 env.hosts = ["54.234.93.141", "34.207.154.98"]
 env.user = "ubuntu"
 env.key_filename = "~/.ssh/id_rsa"
+
+
+def do_pack():
+    """This function generates a .tgz archive from the
+    contents of the web_static folder and stores it in
+    the version folder
+    """
+    date_format = datetime.now().strftime("%Y%m%d%H%M%S")
+    file_path = "versions/web_static_{}.tgz".format(date_format)
+    local("mkdir -p versions")
+    archive = local("tar -cvzf {} web_static".format(file_path))
+
+    if archive.failed:
+        return None
+
+    return file_path
 
 
 def do_deploy(archive_path):
