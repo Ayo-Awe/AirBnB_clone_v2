f39105b0447a6e7184208915a0508b863c59581c
Awe Ayomidipupo <pupoawe@gmail.com>
2023-04-20 20:18:24 +0100

    feat(framework): setup simple flask server

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module contains a simple flask
+server application
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.get("/", strict_slashes=False)
+def root():
+    """Route handler for web server root
+    """
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
---
+++ web_flask/4-number_route.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""This module contains a simple flask
+server application
+"""
+from flask import Flask, escape
+app = Flask(__name__)
+
+
+@app.get("/", strict_slashes=False)
+def root():
+    """Route handler for root of
+    web server
+    """
+    return "Hello HBNB!"
+
+
+@app.get("/hbnb", strict_slashes=False)
+def hbnb():
+    """Route handler for /hbnb endpoint
+    """
+    return "HBNB"
+
+
+@app.get("/c/<text>", strict_slashes=False)
+def c_handler(text):
+    """Route handler for /c endpoint
+    """
+    return "C {}".format(escape(text.replace("_", " ")))
+
+
+@app.get("/python", strict_slashes=False)
+@app.get("/python/<text>", strict_slashes=False)
+def python_handler(text="is_cool"):
+    """Route handler for /python endpoint
+    """
+    return "Python {}".format(escape(text.replace("_", " ")))
+
+
+@app.get("/number/<int:n>", strict_slashes=False)
+def number_handler(n):
+    """Route handler for /c endpoint
+    """
+    return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
