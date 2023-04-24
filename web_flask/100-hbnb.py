d585b5716cf178e196ff9b667a35f47b18a6e396
Awe Ayomidipupo <pupoawe@gmail.com>
2023-04-24 04:39:20 +0100

    feat: solve advance task and make hbnb live

---
+++ web_flask/100-hbnb.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""This module contains a simple flask
+server application. The flask server
+listens on PORT 5000, 0.0.0.0
+
+This server serves data from the storage engine
+and renders it using jinja
+"""
+
+from flask import Flask, escape, render_template
+from models import storage
+import models
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown(c):
+    """Teardown function is called after each
+    request
+
+    This function is called after every request
+    and it closes the current storage session
+    """
+    storage.close()
+
+
+@app.route("/hbnb", strict_slashes=False)
+def serve_hbnb():
+    """Route handler for states endpoint
+    it responds with html template
+
+    This endpoint serves data from the storage engine
+    and renders it using jinja
+    """
+
+    states = list(storage.all(models.classes["State"]).values())
+    amenities = list(storage.all(models.classes["Amenity"]).values())
+    places = list(storage.all(models.classes["Place"]).values())
+
+    return render_template("100-hbnb.html", states=states, amenities=amenities, places=places)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
